50d4dd0f5efa7c0e0c68ec02fbf364e9d1b55fc3
2smallears <2smallears@users.noreply.github.com>
2016-07-31 19:40:25 +0800

    display todo list

--- src/index.js
+++ src/index.js
@@ -1,18 +1,60 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
+let todos = [
+    {
+        task: 'React',
+        completed: false
+    },
+    {
+        task: 'express',
+        completed: false
+    },
+    {
+        task: 'javascript',
+        completed: true
+    }
+];
 
-    render: function render() {
+const TodoList = React.createClass({
+    render() {
         return (
             <div>
-                <h1>ToDos</h1>
-                <input type="text" placeholder="what to be done?" name="todo"/>
+                {todos.map(function(todo, i) {
+                    var className = "";
+                    if (todo.completed) {
+                        className = "completed";
+                    }
+                    return <li className={className} key={i}>
+                        <input type="text" value={todo.task} /><button>X</button>
+                    </li>
+                })}
+            </div>
+        );
+    }
+});
+const TodoForm = React.createClass ({
+    render() {
+        return (
+            <div className="todoForm">
             </div>
         );
     }
 });
 
+const Todo = React.createClass({
+    render: function render() {
+        return (
+            <ul>
+                <li><h1>Todos</h1></li>
+                <li><input type="text" placeholder="what to be done?" id="todos"/></li>
+                <li><TodoList /></li>
+                <li><TodoForm /></li>
+            </ul>
+        );
+    }
+});
+
 ReactDOM.render(
-    <Hello/>, document.getElementById('AppRoot')
+    <Todo/>, document.getElementById('AppRoot')
 );
